68b3ca6ea9b053e5f1709a6aea2c0e099a0930cc
Anupama Garani <anupamagarani95@gmail.com>
2025-02-17 15:14:54 -0600

    Added a notebook about tiktoken encoding types

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, request, jsonify
+import os
+import openai
+import tiktoken
 
 app = Flask(__name__)
+
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+openai.api_key = OPEN_AI_API_KEY
+
+
+def count_tokens(prompt):
+    try:
+        encoding = tiktoken.get_encoding("cl100k_base")
+        encoding = tiktoken.encoding_for_model("gpt-4")
+        num_tokens = len(encoding.encode(prompt))
+        return num_tokens
+    except:
+        print("Error: Could not encode prompt")
+        return len(prompt.split())
+
+
 
 @app.route("/")
 def home():
@@ -11,12 +30,11 @@ def home():
 def process_prompt():
     data = request.json
     prompt = data.get("prompt","")
-    
-    token_count=len(prompt.split())
-    
+    model = data.get("model","gpt-4")
+    estimated_tokens = count_tokens(prompt)         
     return jsonify({
-        "prompt":prompt,
-        "token_count":token_count
+            "prompt":prompt,
+            "token_count":estimated_tokens,
     })
 
 if __name__ == "__main__":
